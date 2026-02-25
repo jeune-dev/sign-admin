cf7b6330aa503c465e6c5277ad3e0170f8e379fd
Balla221 <balla.beye_220540@groupeiam.com>
2026-02-25 23:07:49 +0000

    Structure partie admin

---
+++ src/service/admin/adminService.js
@@ -0,0 +1,82 @@
+import api, { setToken, setUser, clearAuth } from '../api';
+
+/* Nombre d'indépendants */
+export const nombreIndependant = async () => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+/* Nombre de professionnels */
+export const nombreProfessionnelle = async () => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+/* Nombre de particuliers */
+export const nombreParticulier = async () => {
+  try {
+
+} catch (error) {
+    throw error;
+  }
+};
+
+/* Nombre de contrats */
+export const nombreContrat = async () => {
+  try {
+
+} catch (error) {
+    throw error;
+  }
+};
+
+/* Nombre total d'utilisateurs */
+export const nombreUtilisateur = async () => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+/* Nombre de factures */
+export const nombreFacture = async () => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+/* Liste des utilisateurs */
+export const listeUtilisateur = async () => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+/* Activer utilisateur */
+export const activerUtilisateur = async (id) => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+/* Désactiver utilisateur */
+export const desactiverUtilisateur = async (id) => {
+  try {
+    
+  } catch (error) {
+    throw error;
+  }
+};
