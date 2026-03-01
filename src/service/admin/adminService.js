1ef19448e6d3e9cfae6d321c30556218f315db32
Balla221 <balla.beye_220540@groupeiam.com>
2026-03-01 17:50:07 +0000

    Structuration de la partie admin

--- src/service/admin/adminService.js
+++ src/service/admin/adminService.js
@@ -2,133 +2,78 @@ import api from '../api';
 
 /* Nombre d'indépendants */
 export const nombreIndependant = async () => {
-  try {
-    const response = await api.get('/admin/nombre-independants');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/nombre-independants');
+  return response.data;
 };
 
 /* Nombre de professionnels */
 export const nombreProfessionnelle = async () => {
-  try {
-    const response = await api.get('/admin/nombre-professionnels');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/nombre-professionnels');
+  return response.data;
 };
 
 /* Nombre de particuliers */
 export const nombreParticulier = async () => {
-  try {
-    const response = await api.get('/admin/nombre-particuliers');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/nombre-particuliers');
+  return response.data;
 };
 
 /* Nombre de contrats */
 export const nombreContrat = async () => {
-  try {
-    const response = await api.get('/admin/nombre-contrats');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/nombre-contrats');
+  return response.data;
 };
 
 /* Nombre total d'utilisateurs */
 export const nombreUtilisateur = async () => {
-  try {
-    const response = await api.get('/admin/nombre-utilisateur');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/nombre-utilisateur');
+  return response.data;
 };
-
 
 /* Liste des factures */
 export const listeFactures = async () => {
-  try {
-    const response = await api.get('/admin/liste-factures');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/liste-factures');
+  return response.data;
 };
-
 
 /* Nombre de factures */
 export const nombreFacture = async () => {
-  try {
-    const response = await api.get('/admin/nombre-facture');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/nombre-facture');
+  return response.data;
 };
 
 /* Liste des utilisateurs */
 export const listeUtilisateurs = async () => {
-  try {
-    const response = await api.get('/admin/liste-utilisateur');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/liste-utilisateur');
+  return response.data;
 };
 
 /* Liste des administrateurs */
 export const listerAdmins = async () => {
-  try {
-    const response = await api.get('/admin/liste-admins');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/liste-admins');
+  return response.data;
 };
 
-/* Ajouter  des administrateurs */
+/* Ajouter des administrateurs */
 export const ajoutAdmins = async (adminData) => {
-  try {
-    const response = await api.post('/admin/ajout-admins', adminData);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.post('/admin/ajout-admins', adminData);
+  return response.data;
 };
 
-/* Nombre d'adminidtrateurs' */
+/* Nombre d'administrateurs */
 export const nombreAdmin = async () => {
-  try {
-    const response = await api.get('/admin/nombre-admins');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get('/admin/nombre-admins');
+  return response.data;
 };
 
 /* Activer utilisateur */
 export const activerUtilisateur = async (id) => {
-  try {
-    const response = await api.patch(`/admin/activer-utilisateur/${id}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.patch(`/admin/activer-utilisateur/${id}`);
+  return response.data;
 };
 
 /* Désactiver utilisateur */
 export const desactiverUtilisateur = async (id) => {
-  try {
-    const response = await api.patch(`/admin/desactiver-utilisateur/${id}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.patch(`/admin/desactiver-utilisateur/${id}`);
+  return response.data;
 };
-
